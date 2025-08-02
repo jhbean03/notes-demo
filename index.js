b2de9dd4678571e07d3d31be36c4823423ee6f44
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-08-02 16:33:36 -0400

    added frontend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors")
 
 app.use(cors())
-
+app.use(express.static("dist"))
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
