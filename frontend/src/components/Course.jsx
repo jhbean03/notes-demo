0ee6074022a7e5bb630e756e840fb108ea70e8d5
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-08-02 17:11:58 -0400

    added frontend source code

---
+++ frontend/src/components/Course.jsx
@@ -0,0 +1,39 @@
+const Header = ({ name }) => {
+  return (
+    <h2>{name}</h2>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part}/>)}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <b>Total of {parts.reduce((acc, current) => acc + current.exercises, 0)} exercises</b>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
